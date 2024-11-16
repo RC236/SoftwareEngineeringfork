d6c19b6ec2bcc66cdb8c5175a49a888e6d43bfc8
RC236 <146297838+RC236@users.noreply.github.com>
2024-11-16 18:35:28 +0000

    C1W1 Work
    Added files to github for assignment

---
+++ C1W1 - Task 1/10818835_Exercise1.cpp
@@ -0,0 +1,12 @@
+// Data space for defining libraries
+#include <iostream>
+using namespace std;
+
+// Variable declaration space
+int Num_1 = 42
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
